8f79d09407d4a5a3be20c504f0d480092f984769
nicol29 <nicolascondrea@gmail.com>
2022-12-22 16:03:53 +0000

    Implement storing of books

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
+const submitButton = document.querySelector('.submit-button');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const checkboxInput = document.querySelector('#read');
+
 const myLibrary = [];
+let numberOfBooks = 0;
 
-const Bob = 15;
-
-function Book() {
-
+function Book(title, author, pages, isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, isRead) {
+  myLibrary[numberOfBooks] = new Book(title, author, pages, isRead);
+
+  numberOfBooks++;
+  console.log(myLibrary);
+}
+
+submitButton.addEventListener('click', () => {
+  addBookToLibrary(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    checkboxInput.checked,
+  );
+
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  checkboxInput.checked = false;
+});
+
+function displayBooks() {
 
 }
