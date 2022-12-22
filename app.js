329047d48b04d83a8b71a08978b0ca23942b0e8a
nicol29 <nicolascondrea@gmail.com>
2022-12-22 17:28:24 +0000

    Populate book cards with their respective object

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const pagesInput = document.querySelector('#pages');
 const checkboxInput = document.querySelector('#read');
+const booksContainer = document.querySelector('.grid-container');
 
 const myLibrary = [];
 let numberOfBooks = 0;
@@ -14,11 +15,37 @@ function Book(title, author, pages, isRead) {
   this.isRead = isRead;
 }
 
+function displayBooks() {
+  const outputBookDiv = document.createElement('div');
+  outputBookDiv.classList.add('output');
+
+  const titleH2 = document.createElement('h2');
+  titleH2.innerText = myLibrary[numberOfBooks].title;
+  outputBookDiv.appendChild(titleH2);
+
+  const divAuthor = document.createElement('div');
+  divAuthor.innerText = `Author: ${myLibrary[numberOfBooks].author}`;
+  outputBookDiv.appendChild(divAuthor);
+
+  const divPages = document.createElement('div');
+  divPages.innerText = `No. of Pages: ${myLibrary[numberOfBooks].pages}`;
+  outputBookDiv.appendChild(divPages);
+
+  const divRead = document.createElement('div');
+  divRead.innerText = `Finshed Reading: ${myLibrary[numberOfBooks].isRead}`;
+  outputBookDiv.appendChild(divRead);
+
+  booksContainer.appendChild(outputBookDiv);
+
+  numberOfBooks += 1;
+  // myLibrary.forEach((book) => console.log(book));
+}
+
 function addBookToLibrary(title, author, pages, isRead) {
   myLibrary[numberOfBooks] = new Book(title, author, pages, isRead);
 
-  numberOfBooks++;
-  console.log(myLibrary);
+  displayBooks();
+  // console.log(myLibrary);
 }
 
 submitButton.addEventListener('click', () => {
@@ -34,7 +61,3 @@ submitButton.addEventListener('click', () => {
   pagesInput.value = '';
   checkboxInput.checked = false;
 });
-
-function displayBooks() {
-
-}
